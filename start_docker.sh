5a322ba8db93dbdb5e1e653f849c89e9cca93bb5
Ubuntu <ubuntu@ip-172-31-47-63.us-east-2.compute.internal>
2021-03-22 02:13:03 +0000

    update mar21 night

--- start_docker.sh
+++ start_docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run --runtime=nvidia -p 8888:8888 -v /home/ubuntu/pollutemenot-ai:/pmn-ai -v /home/ubuntu/data:/data -v /proc:/proc_main -ti --gpus all -d trainer_1
+docker run --runtime=nvidia -p 8888:8888 -v /home/ubuntu/data/pollutemenot-ai:/pmn-ai -v /home/ubuntu/data:/data -v /proc:/proc_main -ti --gpus all -d trainer_1
 
